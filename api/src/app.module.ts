a22a65ab2aef312b78e2016065922bdda8dd2752
efren sandoval <efren.sandoval@epn.edu.ec>
2020-07-21 17:04:03 -0500

    typeorm module integrado y configurado

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm'
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ActivosModule } from './activos/activos.module';
+import { from } from 'rxjs';
+import { rootCertificates } from 'tls';
 
 @Module({
-  imports: [ActivosModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql'
+      host: 'localhost',
+      port: 32769,
+      username: 'root',
+      password: '12345678'
+      database: 'activos_fijos',
+    }),
+    ActivosModule],
   controllers: [AppController],
   providers: [AppService],
 })
